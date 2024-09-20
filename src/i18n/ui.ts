a38725855ea59c906eae1cd14012359019c153bc
EudaX400 <eudax400@gmail.com>
2024-09-20 13:10:49 +0200

    aplicar traducciones

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,27 +5,3 @@ export const languages = {
 };
 
 export const defaultLang = "ca";
-
-export const ui = {
-  ca: {
-    "nav.experience": "Experiència",
-    "nav.projects": "Projectes",
-    "nav.skills": "Habilitats",
-    "nav.about": "Sobre mi",
-    "nav.contact": "Contacte",
-  },
-  es: {
-    "nav.experience": "Experiéncia",
-    "nav.projects": "Proyectos",
-    "nav.skills": "Habilidades",
-    "nav.about": "Sobre mí",
-    "nav.contact": "Contacto",
-  },
-  en: {
-    "nav.experience": "Experience",
-    "nav.projects": "Projects",
-    "nav.skills": "Skills",
-    "nav.about": "About",
-    "nav.contact": "Contact",
-  },
-} as const;
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,21 +1,43 @@
-import { ui, defaultLang } from './ui';
+// i18n/utils.ts
+import { defaultLang } from './ui';
+import catalan from '../i18n/locales/ca.json';
+import english from '../i18n/locales/en.json';
+import spanish from '../i18n/locales/es.json';
+
+const translations = {
+  ca: catalan,
+  en: english,
+  es: spanish,
+};
 
 export const showDefaultLang = false;
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
-  if (lang in ui) return lang as keyof typeof ui;
+  if (lang in translations) return lang as keyof typeof translations;
   return defaultLang;
 }
 
-export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
-  }
+export function useTranslations(lang: keyof typeof translations) {
+  // Función de traducción que acepta claves de tipo string
+  return function t(key: string) {
+    const keys = key.split('.') as Array<string>;
+    let result: any = translations[lang];
+    let defaultResult: any = translations[defaultLang];
+
+    // Iterar sobre las claves anidadas
+    for (const k of keys) {
+      result = result?.[k];
+      defaultResult = defaultResult?.[k];
+    }
+
+    // Retorna el valor traducido o el valor en el idioma por defecto
+    return result || defaultResult;
+  };
 }
 
-export function useTranslatedPath(lang: keyof typeof ui) {
+export function useTranslatedPath(lang: keyof typeof translations) {
   return function translatePath(path: string, l: string = lang) {
-    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`
-  }
+    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`;
+  };
 }
